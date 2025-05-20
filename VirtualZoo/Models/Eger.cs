959cd795b7daec0bf5f4e2b14674e1cd445ec397
13asTerem <you@example.com>
2025-05-20 18:16:09 +0200

    Eger

--- VirtualZoo/Models/Eger.cs
+++ VirtualZoo/Models/Eger.cs
@@ -6,7 +6,15 @@ using System.Threading.Tasks;
 
 namespace VirtualZoo.Models
 {
-    public class Eger
+    public class Eger : Animal
     {
+        public Eger(string name, int age, string species, string zoneName) : base(name, age, species, zoneName)
+        {
+        }
+
+        public override void MakeSound()
+        {
+            Console.WriteLine($"{Name} cincog: Cin-cin!");
+        }
     }
 }
