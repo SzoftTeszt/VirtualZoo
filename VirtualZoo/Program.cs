3619d9272edf0320038a242d86684438de649938
Erős István <eros@zold.lan>
2025-05-20 19:12:56 +0200

    XXX

--- VirtualZoo/Program.cs
+++ VirtualZoo/Program.cs
@@ -13,6 +13,7 @@ namespace VirtualZoo
             var denever = new Bat("Rezső", 5, "Barlang");
             Console.WriteLine(denever);
             Console.WriteLine(denever.Age>4?"Vén dög!":"Kis pocok!");
+            Console.WriteLine("Itt ütközés lesz :'(");
             //Console.OutputEncoding = System.Text.Encoding.UTF8;
             //ConsoleKeyInfo input = new ConsoleKeyInfo();
 
