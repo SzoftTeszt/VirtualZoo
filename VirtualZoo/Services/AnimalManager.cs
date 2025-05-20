3327137de1a311f99b32791c1fc654ef69487d88
Erős István <eros@zold.lan>
2025-05-20 19:34:16 +0200

    AnimalManager

---
+++ VirtualZoo/Services/AnimalManager.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VirtualZoo.Models;
+
+namespace VirtualZoo.Services
+{
+    public class AnimalManager : IAnimalManager
+    {
+        private List<Animal> animals = new List<Animal>();
+        public void AddAnimal(Animal animal) { }
+
+        public void FeedAnimal(string name) { }
+        public void MoveAnimal(string name, string newZone) { }
+        public Animal GetAnimalByName(string name) {
+            return new Eger("Ernő", 5, "Kamra");
+        }
+        public List<Animal> GetAllAnimals() {
+            return new List<Animal>();
+        }
+        public List<Animal> GetAnimalsByZone(string zoneName) {
+        
+            return new List<Animal>();
+        }
+        public void RemoveAnimal(string name) { }
+
+    }
+}
