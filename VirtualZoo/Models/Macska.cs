8e10e2deeb425b7ffd878efe0392490b64fa50f9
Sebastian Varga <xAzta@users.noreply.github.com>
2025-05-20 18:14:53 +0200

    Add files via upload

--- VirtualZoo/Models/Macska.cs
+++ VirtualZoo/Models/Macska.cs
@@ -6,7 +6,14 @@ using System.Threading.Tasks;
 
 namespace VirtualZoo.Models
 {
-    internal class Cica
+    internal class Macska : Animal
     {
+        public Macska(string name, int age, string species, string zoneName) : base(name, age, species, zoneName)
+        {
+        }
+        public override void MakeSound()
+        {
+            Console.WriteLine($"{Name} ({Species}) nyávog.");
+        }
     }
 }
