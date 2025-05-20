d87220f7b900c1cd3b8ba1138c10eb586823c0e5
13asTerem <you@example.com>
2025-05-20 19:11:58 +0200

    vidraprog

--- VirtualZoo/Program.cs
+++ VirtualZoo/Program.cs
@@ -13,6 +13,12 @@ namespace VirtualZoo
             var denever = new Bat("Rezső", 5, "Barlang");
             Console.WriteLine(denever);
             Console.WriteLine(denever.Age>4?"Vén dög!":"Kis pocok!");
+
+
+            var vidra = new Otter("Áspis", 2, "Tavak");
+            Console.WriteLine(vidra);
+            Console.WriteLine(vidra.Age > 2 ? "Selyem bubó!" : "Nyammogi!");
+
             //Console.OutputEncoding = System.Text.Encoding.UTF8;
             //ConsoleKeyInfo input = new ConsoleKeyInfo();
 
@@ -31,7 +37,7 @@ namespace VirtualZoo
             //    Console.WriteLine("5 - Állat mozgatása");
             //    Console.WriteLine("6 - Állat törlése");
             //    Console.WriteLine("0 - Kilépés\n");
-  
+
             //    switch (input.KeyChar)
             //    {
             //        case '1': AddAnimal(); break;
